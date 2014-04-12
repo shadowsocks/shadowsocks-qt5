a706743a4715b03edfa6b993a150d1344bc48f76
William Wong <hzwhuang@gmail.com>
2014-04-12 16:41:26 +0800

    typo.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -391,5 +391,5 @@ void MainWindow::checkIfSaved()
 
 void MainWindow::aboutButtonClicked()
 {
-    QMessageBox::about(this, QString("About Shadowsocks-Qt5"), QString("<h3>Platform-Cross GUI Client for Shadowsocks.</h3><p>Version: 0.2.0</p><p>Licensed Under LGPLv3<br />This software is hosted at <a href='https://github.com/librehat/shadowsocks-qt5'>GitHub</a>.</p>"));
+    QMessageBox::about(this, QString("About Shadowsocks-Qt5"), QString("<h3>Platform-Cross GUI Client for Shadowsocks.</h3><p>Version: 0.2.0</p><p>Licensed under LGPLv3<br />This software is hosted at <a href='https://github.com/librehat/shadowsocks-qt5'>GitHub</a>.</p>"));
 }
