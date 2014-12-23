362665933a331cdfadea2836f3c98ac74ce83343
wzhy90 <wzhy90@gmail.com>
2014-12-23 19:22:57 +0800

    Update Simplified Chinese translation.

--- src/i18n/ssqt5_zh_CN.ts
+++ src/i18n/ssqt5_zh_CN.ts
@@ -27,6 +27,11 @@
 <context>
     <name>MainWindow</name>
     <message>
+        <location filename="../mainwindow.ui" line="26"/>
+        <source>Control Panel</source>
+        <translation>控制面板</translation>
+    </message>
+    <message>
         <location filename="../mainwindow.ui" line="41"/>
         <source>General</source>
         <translation>通用</translation>
@@ -37,28 +42,29 @@
         <translation>本地端口</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="83"/>
-        <source>Server Port</source>
-        <translation>服务器端口</translation>
-    </message>
-    <message>
         <location filename="../mainwindow.ui" line="66"/>
         <source>Password</source>
         <translation>密钥</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="132"/>
-        <source>Timeout</source>
-        <translation>超时</translation>
-    </message>
-    <message>
-        <source>Server Domain/IP</source>
-        <translation type="vanished">服务器域名/IP</translation>
+        <location filename="../mainwindow.ui" line="83"/>
+        <source>Server Port</source>
+        <translation>服务器端口</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="96"/>
         <source>Server Address</source>
         <translation>服务器地址</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="125"/>
+        <source>Local Address</source>
+        <translation>本地地址</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="132"/>
+        <source>Timeout</source>
+        <translation>超时</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="139"/>
@@ -71,83 +77,6 @@
         <translation>加密方式</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="125"/>
-        <source>Local Address</source>
-        <translation>本地地址</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="193"/>
-        <source>Shadowsocks-NodeJS</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="308"/>
-        <location filename="../mainwindow.cpp" line="61"/>
-        <source>Start</source>
-        <translation>启动</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="320"/>
-        <location filename="../mainwindow.cpp" line="62"/>
-        <source>Stop</source>
-        <translation>停止</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="230"/>
-        <source>+</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="277"/>
-        <source>Profile</source>
-        <translation>配置</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="265"/>
-        <source>-</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="211"/>
-        <source>Backend</source>
-        <translation>后端</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="218"/>
-        <source>...</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="242"/>
-        <source>Backend Type</source>
-        <translation>后端类型</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="188"/>
-        <source>Shadowsocks-libev</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="485"/>
-        <source>For portable purpose</source>
-        <translation>适用于便携版</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="488"/>
-        <source>Use relative path instead of absolute path</source>
-        <translation>使用相对路径而不是绝对路径</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="198"/>
-        <source>Shadowsocks-Go</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="26"/>
-        <source>Control Panel</source>
-        <translation>控制面板</translation>
-    </message>
-    <message>
         <location filename="../mainwindow.ui" line="169"/>
         <source>Only available in Linux with Kernel &gt;= 3.7
 Not every port of Shadowsocks support this option.</source>
@@ -155,187 +84,139 @@ Not every port of Shadowsocks support this option.</source>
 不是所有版本的Shadowsocks都支持该选项。</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="203"/>
-        <source>Shadowsocks-Python</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="287"/>
-        <source>Custom Arguments</source>
-        <translation>自定义参数</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="345"/>
-        <source>Share</source>
-        <translation>分享</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="365"/>
-        <source>Log</source>
-        <translation>日志</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="395"/>
-        <source>Misc</source>
-        <translation>杂项</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="408"/>
-        <source>Start backend automatically when started and the current profile is valid.</source>
-        <translation>启动后如果当前配置有效则自动启动后端。</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="401"/>
-        <source>Use translucent background</source>
-        <translation>使用透明背景</translation>
-    </message>
-    <message>
-        <source>Start backend automatically when ss-qt5 started if current profile is valid.</source>
-        <translation type="vanished">当ss-qt5启动后如果当前配置有效则自动启动后端</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="411"/>
-        <source>Autostart if profile is valid</source>
-        <translation>配置有效则自动启动</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="478"/>
-        <source>Debug-level log</source>
-        <translation>除错级别的日志</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="434"/>
-        <source>Save</source>
-        <translation>保存</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="446"/>
-        <location filename="../mainwindow.h" line="79"/>
-        <source>About</source>
-        <translation>关于</translation>
-    </message>
-    <message>
-        <source>Hide main window automatically when ss-qt5 started.</source>
-        <translation type="vanished">当ss-qt5启动后自动隐藏主窗口</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="458"/>
-        <source>Auto hide main window when started</source>
-        <translation>启动后自动隐藏主窗口</translation>
-    </message>
-    <message>
-        <source>Only available in Linux with Kernel &gt; 3.7
-Not every port of Shadowsocks support this option.</source>
-        <translation type="vanished">仅在Linux内核版本大于3.7下可用
-并非每个Shadowsocks的port都支持此选项。</translation>
-    </message>
-    <message>
         <location filename="../mainwindow.ui" line="173"/>
         <source>Use TCP Fast Open if available</source>
         <translation>使用TCP Fast Open（如果可用）</translation>
     </message>
     <message>
-        <source>Show/Hide</source>
-        <translation type="vanished">显示/隐藏</translation>
+        <location filename="../mainwindow.ui" line="188"/>
+        <source>Shadowsocks-libev</source>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="60"/>
-        <source>Show</source>
-        <translation>显示</translation>
+        <location filename="../mainwindow.ui" line="193"/>
+        <source>Shadowsocks-NodeJS</source>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="63"/>
-        <source>Exit</source>
-        <translation>退出</translation>
+        <location filename="../mainwindow.ui" line="198"/>
+        <source>Shadowsocks-Go</source>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="349"/>
-        <source>Profile: %1 Started</source>
-        <translation>配置： %1 已启动</translation>
+        <location filename="../mainwindow.ui" line="203"/>
+        <source>Shadowsocks-Python</source>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="365"/>
-        <source>Profile: %1 Stopped</source>
-        <translation>配置： %1 已停止</translation>
+        <location filename="../mainwindow.ui" line="208"/>
+        <source>libQtShadowsocks</source>
+        <translation></translation>
     </message>
     <message>
-        <source>Shadowsocks started! profile: %1.</source>
-        <translation type="vanished">Shadowsocks 已启动！配置： %1。</translation>
+        <location filename="../mainwindow.ui" line="216"/>
+        <source>Backend</source>
+        <translation>后端</translation>
     </message>
     <message>
-        <source>Shadowsocks stoped!</source>
-        <translation type="vanished">Shadowsocks 已停止！</translation>
+        <location filename="../mainwindow.ui" line="223"/>
+        <source>...</source>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="393"/>
-        <source>Save Changes</source>
-        <translation>保存变更</translation>
+        <location filename="../mainwindow.ui" line="235"/>
+        <source>+</source>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="393"/>
-        <source>Configuration has been changed.
-Do you want to save it now?</source>
-        <translation>设置已经被变更。
-你想要现在保存吗？</translation>
+        <location filename="../mainwindow.ui" line="247"/>
+        <source>Backend Type</source>
+        <translation>后端类型</translation>
     </message>
     <message>
-        <source>&lt;h3&gt;Platform-Cross GUI Client for Shadowsocks&lt;/h3&gt;&lt;p&gt;Version: 0.3.1&lt;/p&gt;&lt;p&gt;Copyright © 2014 William Wong (&lt;a href=&apos;https://twitter.com/librehat&apos;&gt;@librehat&lt;/a&gt;)&lt;/p&gt;&lt;p&gt;Licensed under LGPLv3&lt;br /&gt;Project Hosted at &lt;a href=&apos;https://github.com/librehat/shadowsocks-qt5&apos;&gt;GitHub&lt;/a&gt;&lt;/p&gt;</source>
-        <translation type="vanished">&lt;h3&gt;Shadowsocks的跨平台图形化客户端&lt;/h3&gt;&lt;p&gt;版本： 0.3.1&lt;/p&gt;&lt;p&gt;版权所有 © 2014 William Wong (&lt;a href=&apos;https://twitter.com/librehat&apos;&gt;@librehat&lt;/a&gt;)&lt;/p&gt;&lt;p&gt;以LGPLv3许可证发布&lt;br /&gt;项目托管在&lt;a href=&apos;https://github.com/librehat/shadowsocks-qt5&apos;&gt;GitHub&lt;/a&gt;&lt;/p&gt;</translation>
+        <location filename="../mainwindow.ui" line="270"/>
+        <source>-</source>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="312"/>
-        <source>Error</source>
-        <translation>错误</translation>
+        <location filename="../mainwindow.ui" line="282"/>
+        <source>Profile</source>
+        <translation>配置</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="312"/>
-        <source>Invalid profile or configuration.</source>
-        <translation>无效的配置或设置。</translation>
+        <location filename="../mainwindow.ui" line="292"/>
+        <source>Custom Arguments</source>
+        <translation>自定义参数</translation>
     </message>
     <message>
-        <source>Unsaved Profile</source>
-        <translation type="vanished">配置未保存</translation>
+        <location filename="../mainwindow.ui" line="313"/>
+        <source>Start</source>
+        <translation>启动</translation>
     </message>
     <message>
-        <source>Current profile is not saved yet.
-Do you want to save it now?</source>
-        <translation type="vanished">当前的配置暂未保存。
-你希望现在就保存吗？</translation>
+        <location filename="../mainwindow.ui" line="325"/>
+        <source>Stop</source>
+        <translation>停止</translation>
     </message>
     <message>
-        <source>About Shadowsocks-Qt5</source>
-        <translation type="vanished">关于Shadowsocks-Qt5</translation>
-    </message>
-</context>
-<context>
-    <name>QRWidget</name>
-    <message>
-        <location filename="../qrwidget.cpp" line="48"/>
-        <source>Generating QR code failed.</source>
-        <translation>二维码生成失败。</translation>
-    </message>
-</context>
-<context>
-    <name>SS_Process</name>
-    <message>
-        <location filename="../ss_process.cpp" line="54"/>
-        <source>Aborted: Invalid Backend Type.</source>
-        <translation>终止：无效的后端类型。</translation>
+        <location filename="../mainwindow.ui" line="350"/>
+        <source>Share</source>
+        <translation>分享</translation>
     </message>
     <message>
-        <location filename="../ss_process.cpp" line="62"/>
-        <source>Backend arguments are </source>
-        <translation>后端参数为</translation>
+        <location filename="../mainwindow.ui" line="372"/>
+        <source>Log</source>
+        <translation>日志</translation>
     </message>
     <message>
-        <location filename="../ss_process.cpp" line="115"/>
-        <source>Backend started. PID: </source>
-        <translation>后端已启动。PID：</translation>
+        <location filename="../mainwindow.ui" line="411"/>
+        <source>Misc</source>
+        <translation>杂项</translation>
     </message>
     <message>
-        <location filename="../ss_process.cpp" line="121"/>
-        <source>Backend exited. Exit Code: </source>
-        <translation>后端已退出。退出代号：</translation>
+        <location filename="../mainwindow.ui" line="417"/>
+        <source>Use translucent background</source>
+        <translation>使用透明背景</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="424"/>
+        <source>Start backend automatically when started and the current profile is valid.</source>
+        <translation>启动后如果当前配置有效则自动启动后端。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="427"/>
+        <source>Autostart if profile is valid</source>
+        <translation>配置有效则自动启动</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="450"/>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="462"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="474"/>
+        <source>Auto hide main window when started</source>
+        <translation>启动后自动隐藏主窗口</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="494"/>
+        <source>Debug-level log</source>
+        <translation>除错级别的日志</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="501"/>
+        <source>For portable purpose</source>
+        <translation>适用于便携版</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="504"/>
+        <source>Use relative path instead of absolute path</source>
+        <translation>使用相对路径而不是绝对路径</translation>
     </message>
 </context>
 <context>
