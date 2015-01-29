780f2fbe5e86939b7103360b59feed862f2940ac
Symeon Huang <hzwhuang@gmail.com>
2015-01-29 15:33:48 +0000

    update translations

--- src/i18n/ss-qt5_zh_CN.ts
+++ src/i18n/ss-qt5_zh_CN.ts
@@ -170,141 +170,140 @@ Not every port of Shadowsocks support this option.</source>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="309"/>
-        <location filename="../mainwindow.cpp" line="65"/>
+        <location filename="../mainwindow.cpp" line="356"/>
         <source>Start</source>
         <translation>启动</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="321"/>
-        <location filename="../mainwindow.cpp" line="66"/>
+        <location filename="../mainwindow.ui" line="319"/>
+        <location filename="../mainwindow.cpp" line="357"/>
         <source>Stop</source>
         <translation>停止</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="346"/>
+        <location filename="../mainwindow.ui" line="342"/>
         <source>Share</source>
         <translation>分享</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="372"/>
+        <location filename="../mainwindow.ui" line="366"/>
         <source>Reset</source>
         <translation>重置</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="415"/>
+        <location filename="../mainwindow.ui" line="409"/>
         <source>Log</source>
         <translation>日志</translation>
     </message>
     <message>
-        <source>Misc</source>
-        <translation type="vanished">杂项</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="460"/>
+        <location filename="../mainwindow.ui" line="454"/>
         <source>Use translucent background</source>
         <translation>使用透明背景</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="467"/>
+        <location filename="../mainwindow.ui" line="461"/>
         <source>Start backend automatically when started and the current profile is valid.</source>
         <translation>启动后如果当前配置有效则自动启动后端。</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="470"/>
+        <location filename="../mainwindow.ui" line="464"/>
         <source>Autostart if profile is valid</source>
         <translation>配置有效则自动启动</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="565"/>
+        <location filename="../mainwindow.ui" line="548"/>
+        <source>This will enable application minimise to system tray.
+If system tray icon doesn&apos;t show or malfunction, please uncheck this option.
+Need application restart to take effect.</source>
+        <translation>该选项使得程序能最小化到系统托盘。
+如果系统托盘不显示或者异常，请取消勾选本项。
+程序需要重新启动以生效。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="560"/>
         <source>Allow only one instance running</source>
         <translation>仅允许一个实例运行</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="400"/>
-        <location filename="../mainwindow.ui" line="493"/>
+        <location filename="../mainwindow.ui" line="394"/>
+        <location filename="../mainwindow.ui" line="487"/>
         <source>Save</source>
         <translation>保存</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="454"/>
+        <location filename="../mainwindow.ui" line="448"/>
         <source>Miscellaneous</source>
         <translation>杂项</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="505"/>
-        <location filename="../mainwindow.h" line="80"/>
+        <location filename="../mainwindow.ui" line="499"/>
+        <location filename="../mainwindow.h" line="89"/>
         <source>About</source>
         <translation>关于</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="517"/>
+        <location filename="../mainwindow.ui" line="511"/>
         <source>Auto hide main window when started</source>
         <translation>启动后自动隐藏主窗口</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="537"/>
+        <location filename="../mainwindow.ui" line="531"/>
         <source>Debug-level log</source>
         <translation>除错级别的日志</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="544"/>
+        <location filename="../mainwindow.ui" line="538"/>
         <source>For portable purpose</source>
         <translation>适用于便携版</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="547"/>
+        <location filename="../mainwindow.ui" line="541"/>
         <source>Use relative path instead of absolute path</source>
         <translation>使用相对路径而不是绝对路径</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="554"/>
-        <source>This will enable application minimise to system tray.
-If system tray icon doesn&apos;t show or malfunction, please uncheck this option.</source>
-        <translation>该选项使得程序能最小化到系统托盘
-如果系统托盘不显示或者异常，请取消勾选本项。</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="558"/>
+        <location filename="../mainwindow.ui" line="553"/>
         <source>Use system tray</source>
         <translation>使用系统托盘</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="64"/>
+        <location filename="../mainwindow.cpp" line="337"/>
+        <location filename="../mainwindow.cpp" line="355"/>
         <source>Show</source>
         <translation>显示</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="67"/>
-        <source>Exit</source>
-        <translation>退出</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.cpp" line="310"/>
+        <location filename="../mainwindow.cpp" line="305"/>
         <source>Error</source>
         <translation>错误</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="310"/>
+        <location filename="../mainwindow.cpp" line="305"/>
         <source>Invalid profile or configuration.</source>
         <translation>无效的配置或设置。</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="347"/>
+        <location filename="../mainwindow.cpp" line="358"/>
+        <source>Quit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="405"/>
         <source>Profile: %1 Started</source>
         <translation>配置：%1 已启动</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="363"/>
+        <location filename="../mainwindow.cpp" line="413"/>
         <source>Profile: %1 Stopped</source>
         <translation>配置：%1 已停止</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="392"/>
+        <location filename="../mainwindow.cpp" line="442"/>
         <source>Save Changes</source>
         <translation>保存变更</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="392"/>
+        <location filename="../mainwindow.cpp" line="442"/>
         <source>Configuration has been changed.
 Do you want to save it now?</source>
         <translation>设置已经被变更。
@@ -314,12 +313,12 @@ Do you want to save it now?</source>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="36"/>
+        <location filename="../main.cpp" line="35"/>
         <source>Error</source>
         <translation>错误</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="36"/>
+        <location filename="../main.cpp" line="35"/>
         <source>Another instance of Shadowsocks-Qt5 is already running.</source>
         <translation>另一个Shadowsocks-Qt5的实例已经在运行了。</translation>
     </message>
@@ -335,22 +334,22 @@ Do you want to save it now?</source>
 <context>
     <name>SS_Process</name>
     <message>
-        <location filename="../ss_process.cpp" line="59"/>
+        <location filename="../ss_process.cpp" line="66"/>
         <source>Aborted: Invalid Backend Type.</source>
         <translation>终止：无效的后端类型。</translation>
     </message>
     <message>
-        <location filename="../ss_process.cpp" line="67"/>
+        <location filename="../ss_process.cpp" line="74"/>
         <source>Backend arguments are </source>
         <translation>后端参数为</translation>
     </message>
     <message>
-        <location filename="../ss_process.cpp" line="144"/>
+        <location filename="../ss_process.cpp" line="141"/>
         <source>Backend started. PID: </source>
         <translation>后端已启动。PID：</translation>
     </message>
     <message>
-        <location filename="../ss_process.cpp" line="150"/>
+        <location filename="../ss_process.cpp" line="147"/>
         <source>Backend exited. Exit Code: </source>
         <translation>后端已退出。退出代码：</translation>
     </message>
