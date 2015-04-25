ba0c1e9b6ca0657698bb50b0ff0f11536f24cf5a
Symeon Huang <hzwhuang@gmail.com>
2015-04-25 15:10:42 +0100

    use default constructor so that we can get all information

--- src/controllerthread.cpp
+++ src/controllerthread.cpp
@@ -12,12 +12,12 @@ void ControllerThread::setup(bool _debug, const QSS::Profile &_profile)
 
 void ControllerThread::run()
 {
-    QSS::Controller controller(profile, true);
+    QSS::Controller controller(true);
     connect(&controller, &QSS::Controller::runningStateChanged, this, &ControllerThread::stateChanged);
     connect(&controller, debug ? &QSS::Controller::debug : &QSS::Controller::error, this, &ControllerThread::logAvailable);
     connect(&controller, &QSS::Controller::newBytesReceived, this, &ControllerThread::newBytesRead);
     connect(&controller, &QSS::Controller::newBytesSent, this, &ControllerThread::newBytesSent);
-
+    controller.setup(profile);
     controller.start();
     exec();
 }
