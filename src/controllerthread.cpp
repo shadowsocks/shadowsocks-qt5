407f144e7af6be4eb985e6797c6e90eaa7524457
Symeon Huang <hzwhuang@gmail.com>
2015-05-04 11:31:00 +0100

    change minimum log level from error to info

--- src/controllerthread.cpp
+++ src/controllerthread.cpp
@@ -14,7 +14,7 @@ void ControllerThread::run()
 {
     QSS::Controller controller(true);
     connect(&controller, &QSS::Controller::runningStateChanged, this, &ControllerThread::stateChanged);
-    connect(&controller, debug ? &QSS::Controller::debug : &QSS::Controller::error, this, &ControllerThread::logAvailable);
+    connect(&controller, debug ? &QSS::Controller::debug : &QSS::Controller::info, this, &ControllerThread::logAvailable);
     connect(&controller, &QSS::Controller::newBytesReceived, this, &ControllerThread::newBytesRead);
     connect(&controller, &QSS::Controller::newBytesSent, this, &ControllerThread::newBytesSent);
     controller.setup(profile);
