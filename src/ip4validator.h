d65e9c96157d7cfb794f061170542a388459817d
Symeon Huang <hzwhuang@gmail.com>
2015-05-21 17:02:12 +0100

    add license headers to all header files

--- src/ip4validator.h
+++ src/ip4validator.h
@@ -1,4 +1,20 @@
 /*
+ * Copyright (C) 2014-2015 Symeon Huang <hzwhuang@gmail.com>
+ *
+ * shadowsocks-qt5 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * shadowsocks-qt5 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libQtShadowsocks; see the file LICENSE. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
  * This class is based on a piece of code posted on Qt Centre.
  * http://www.qtcentre.org/threads/6228-Ip-Address-Validation?p=32368#post32368
  */
--- src/portvalidator.h
+++ src/portvalidator.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2014-2015 Symeon Huang <hzwhuang@gmail.com>
+ *
+ * shadowsocks-qt5 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * shadowsocks-qt5 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libQtShadowsocks; see the file LICENSE. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef PORTVALIDATOR_H
 #define PORTVALIDATOR_H
 
