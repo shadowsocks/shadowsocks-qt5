8257b63e073b0fb2d345979864d574cdc62a083a
Symeon Huang <hzwhuang@gmail.com>
2015-08-26 16:19:29 +0100

    Revert "Do not show window if Start Hiden is on."

--- src/statusnotifier.cpp
+++ src/statusnotifier.cpp
@@ -7,7 +7,7 @@
 #include <QDBusPendingCall>
 #endif
 
-StatusNotifier::StatusNotifier(MainWindow *w, bool startHiden, QObject *parent) :
+StatusNotifier::StatusNotifier(MainWindow *w, QObject *parent) :
     QObject(parent),
     window(w)
 {
@@ -18,7 +18,7 @@ StatusNotifier::StatusNotifier(MainWindow *w, bool startHiden, QObject *parent) 
             this->activate();
         }
     });
-    minimiseRestoreAction = new QAction(startHiden ? tr("Restore") : tr("Minimise"), this);
+    minimiseRestoreAction = new QAction(tr("Minimise"), this);
     connect(minimiseRestoreAction, &QAction::triggered, this, &StatusNotifier::activate);
     systrayMenu.addAction(minimiseRestoreAction);
     systrayMenu.addAction(QIcon::fromTheme("application-exit", QIcon::fromTheme("exit")), tr("Quit"), qApp, SLOT(quit()));
@@ -26,9 +26,9 @@ StatusNotifier::StatusNotifier(MainWindow *w, bool startHiden, QObject *parent) 
 
 #ifdef Q_OS_UNIX
     QString de(getenv("XDG_CURRENT_DESKTOP"));
-    useAppIndicator = appIndicatorDE.contains(de, Qt::CaseInsensitive);
+    useAppIndicator = (appIndicatorDE.contains(de, Qt::CaseInsensitive) && !QT_VERSION_CHECK(5, 4, 2));
     if (useAppIndicator) {
-        createAppIndicator(startHiden);
+        createAppIndicator();
     } else {
 #endif
         systray.show();
@@ -62,13 +62,12 @@ void onQuit(GtkMenu *, gpointer data)
     reinterpret_cast<QApplication *>(data)->quit();
 }
 
-void StatusNotifier::createAppIndicator(bool startHiden)
+void StatusNotifier::createAppIndicator()
 {
     AppIndicator *indicator = app_indicator_new("Shadowsocks-Qt5", "shadowsocks-qt5", APP_INDICATOR_CATEGORY_OTHER);
     GtkWidget *menu = gtk_menu_new();
 
-    minimiseRestoreGtkItem = gtk_menu_item_new_with_label(
-        tr(startHiden ? "Restore" : "Minimise").toLocal8Bit().constData());
+    minimiseRestoreGtkItem = gtk_menu_item_new_with_label(tr("Minimise").toLocal8Bit().constData());
     gtk_menu_shell_append(GTK_MENU_SHELL(menu), minimiseRestoreGtkItem);
     g_signal_connect(minimiseRestoreGtkItem, "activate", G_CALLBACK(onAppIndicatorActivated), window);
     gtk_widget_show(minimiseRestoreGtkItem);
